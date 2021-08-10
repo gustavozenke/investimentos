e1e23516807c3e7178dc5334c094394b6c3edef4
Gustavo Zenke <gustavozenke01@gmail.com>
2021-08-10 05:39:25 -0300

    Testes unitarios consulta de listas de fiis e acoes

---
+++ src/test/java/br/com/zenke/investimentos/service/acao/ConsultarListaAcoesServiceTest.java
@@ -0,0 +1,69 @@
+package br.com.zenke.investimentos.service.acao;
+
+import br.com.zenke.investimentos.models.Acao;
+import br.com.zenke.investimentos.repository.AcaoRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ConsultarListaAcoesServiceTest {
+
+    @InjectMocks
+    private ConsultarListaAcoesService consultarListaAcoesService;
+
+    @Mock
+    private AcaoRepository repository;
+
+    private List<Acao> listaAcoes = new ArrayList<>();
+
+    @Test
+    void testar_consulta_acao() {
+
+        Acao acaoExpected = new Acao("RADL4",
+                new BigDecimal("90.09"),
+                new Date(1997, 9, 17),
+                "Saude",
+                "Raia Drogasil S.A",
+                "ON", "Comercio e distribuicao",
+                "Medicamento e outros produtos",
+                new BigDecimal("4448000000.00"),
+                new BigDecimal("14150000000.00"),
+                new BigDecimal("898380000.00"),
+                new BigDecimal("547730000.00"),
+                12.21f,
+                3.87f,
+                new BigDecimal("44750000000.00"));
+
+        Acao acaoExpected2 = new Acao("ITSA4",
+                new BigDecimal("11.09"),
+                new Date(2004, 11, 21),
+                "Bancario",
+                "Itausa",
+                "PN", "Bancarios",
+                "Financeiro",
+                new BigDecimal("2948005600.14"),
+                new BigDecimal("1150069700.45"),
+                new BigDecimal("983150000.00"),
+                new BigDecimal("532030000.00"),
+                09.00f,
+                4.26f,
+                new BigDecimal("10360000400.00"));
+
+        listaAcoes.add(acaoExpected);
+        listaAcoes.add(acaoExpected2);
+
+        when(repository.findAll()).thenReturn(listaAcoes);
+        HttpStatus httpStatus = consultarListaAcoesService.consultarListaAcoes().getStatusCode();
+
+        Assertions.assertEquals(HttpStatus.OK, httpStatus);
+    }
+}
---
+++ src/test/java/br/com/zenke/investimentos/service/fii/ConsultarListaFiiServiceTest.java
@@ -0,0 +1,62 @@
+package br.com.zenke.investimentos.service.fii;
+
+import br.com.zenke.investimentos.models.Acao;
+import br.com.zenke.investimentos.models.Fii;
+import br.com.zenke.investimentos.repository.AcaoRepository;
+import br.com.zenke.investimentos.repository.FIIRepository;
+import br.com.zenke.investimentos.service.acao.ConsultarListaAcoesService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ConsultarListaFiiServiceTest {
+
+    @InjectMocks
+    private ConsultarListaFIIsService consultarListaFIIsService;
+
+    @Mock
+    private FIIRepository repository;
+
+    private List<Fii> listaFii = new ArrayList<>();
+
+    @Test
+    void testar_consulta_acao() {
+
+        Fii fiiExpected = new Fii("VRTA11",
+                new BigDecimal(126.49),
+                new Date(2017, 10, 20),
+                "ALIANZA TRUST RENDA IMOBILIARIA FDO INV IMOB",
+                "Logística",
+                0.49f,
+                new BigDecimal(0.62),
+                0.0f ,
+                13);
+
+        Fii fiiExpected2 = new Fii("BCFF11",
+                new BigDecimal(83.10),
+                new Date(2009, 8, 27),
+                "BTG Pactual Fundo de Fundos",
+                "Títulos e Val. Mob.",
+                0.56f,
+                new BigDecimal(0.45),
+                0.0f ,
+                0);
+
+        listaFii.add(fiiExpected);
+        listaFii.add(fiiExpected2);
+
+        when(repository.findAll()).thenReturn(listaFii);
+        HttpStatus httpStatus = consultarListaFIIsService.consultarListaFIIs().getStatusCode();
+
+        Assertions.assertEquals(HttpStatus.OK, httpStatus);
+    }
+}
